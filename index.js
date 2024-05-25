a39b368fe7dbbd7dda64befd3645989670bdb7d2
alex <shizofranky@gmail.com>
2024-05-25 16:14:34 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT || 3002
 const app = express();
 const cors = require('cors');
 
